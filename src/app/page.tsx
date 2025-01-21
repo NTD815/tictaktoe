514f85bbdaa5d18073492006472552ba4b3a2d99
manjeetnethues <manjeet.kumar@123789.org>
2025-01-21 19:23:20 +0530

    History fixes, moveset revision and sync, winner determination

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { SquareProps } from "@/types/ttt"
 
 export default function Board() {
@@ -18,14 +18,105 @@ export default function Board() {
 
   const [lastMove, setLastMove] = useState('');
 
+  const [winner, setWinner] = useState('');
+
+  const [currentStep, setCurrentStep] = useState(9);
+
+  const [leadingMark, setLeadingMark] = useState<string>('');
   // console.log(boardState)
   // console.log(lastMove)
-  const handleClick = (rowIdx: number, colIdx: number) => {
 
+  useEffect(() => {
+    if(gameSteps.length >= 5){
+      checkVictoryCondition();
+    }
+  }, [boardState, gameSteps]);
+
+  const syncMoveSetsWithHistoryRevision = () => {
+    if(currentStep === 0){
+        if(leadingMark == 'X'){
+            setXMoveSet(() => {
+                return xMoveSet.slice(0,1);
+            });
+            setOMoveSet(() => {
+                return [];
+            });
+        }else{
+            setXMoveSet(() => {
+                return [];
+            });
+            setOMoveSet(() => {
+                return oMoveSet.slice(0,1);
+            });
+        }
+    }else if(currentStep % 2 != 0){
+        //slice equally  
+        let elements = (currentStep + 1)/2
+
+        setXMoveSet(() => {
+            return xMoveSet.slice(0, elements);
+        });
+
+        setOMoveSet(() => {
+            return oMoveSet.slice(0, elements);
+        });
+    }else if(currentStep % 2 == 0){
+        let leadingSlice = currentStep/2 + 1;
+        let followingSlice = currentStep/2;
+
+        if(leadingMark == 'X'){
+            setXMoveSet(() => {
+                return xMoveSet.slice(0, leadingSlice);
+            });
+
+            setOMoveSet(() => {
+                return oMoveSet.slice(0, followingSlice);
+            });
+        }else{
+            setXMoveSet(() => {
+                return xMoveSet.slice(0, followingSlice);
+            });
+
+            setOMoveSet(() => {
+                return oMoveSet.slice(0, leadingSlice);
+            });
+        }
+    }
+  }
+  const placeMarker = (rowIdx: number, colIdx: number) => {
+
+    let lm = lastMove;
+
+    if(winner) return;
+
+    if(currentStep < gameSteps.length -1){
+      //invalidate the history  after this step
+      setGameSteps((prev) => {
+        return prev.slice(0, currentStep + 1);
+      });
+
+      syncMoveSetsWithHistoryRevision();
+
+      //reset current set since we now want to build new history
+      setCurrentStep(9);
+
+      //reset last move for that step
+      let followUpMark = leadingMark == 'X' ? 'O' : 'X';
+
+      if(currentStep % 2 == 0){
+        setLastMove(leadingMark);
+        lm = leadingMark;
+      }else{
+        setLastMove(followUpMark);
+        lm = followUpMark;
+      }
+    }
     //check if a spot has already been marked, and if so, we don't do any further change
     if(boardState[rowIdx][colIdx]) return;
 
-    let currMove = lastMove == 'X' ? 'O' : 'X'; 
+    let currMove = lm == 'X' ? 'O' : 'X'; 
+    
+    if(!gameSteps.length) setLeadingMark(currMove);
 
     if(currMove == 'X'){
       setXMoveSet((prev) => {
@@ -36,18 +127,15 @@ export default function Board() {
         return [...prev, 3*rowIdx + colIdx];
       });
     }
-
-    console.log(xMoveSet)
-    console.log(oMoveSet)
     
     setBoardState((prev) => {
       const board = [...prev]
 
-      const row = board[rowIdx]
+    //   const row = board[rowIdx]
 
-      row[colIdx] = currMove
+    //   row[colIdx] = currMove
 
-      board[rowIdx] = row
+      board[rowIdx][colIdx] = currMove
 
       return board;
     });
@@ -55,12 +143,10 @@ export default function Board() {
     setLastMove(currMove)
 
     setGameSteps((prev) => {
-      const currState = boardState.map((ele) => [...ele]);
+      const currState = structuredClone(boardState);
       return [...prev, currState];
     })
-
-    console.log(gameSteps)
-  }
+}
 
   const resetBoard = () => {
     setBoardState(() => {
@@ -69,25 +155,89 @@ export default function Board() {
         ['', '', ''],
         ['', '', '']
       ];
+    });
+
+    //reset game history
+    setGameSteps(() => {
+      return [];
+    });
+
+    //reset move sets
+    setXMoveSet(() => {
+      return [];
+    });
+
+    setOMoveSet(() => {
+      return [];
     })
+
+    //reset last move
+    setLastMove('');
+
+    setCurrentStep(9);
+
+    setLeadingMark('');
+
+    setWinner('');
   }
 
   const jumpToStep = (step: number) => {
     setBoardState(() => {
-      return gameSteps[step];
+      return structuredClone(gameSteps[step]);
     });
+
+    setCurrentStep(step);
+  }
+
+  const currentTurn = () => {
+    return lastMove == 'X' ? 'O' : 'X';
+  }
+  const checkVictoryCondition = () => {
+    //check rows
+    for(let i = 0; i < 3; i++){
+      if(boardState[i][0] && boardState[i][0] == boardState[i][1] && boardState[i][0] == boardState[i][2]){
+        setWinner(boardState[i][0]);
+        return;
+      }
+    }
+
+    //check columns
+    for(let i = 0; i < 3; i++){
+        if(boardState[0][i] && boardState[0][i] == boardState[1][i] && boardState[0][i] == boardState[2][i]){
+          setWinner(boardState[0][i]);
+          return;
+        }
+    }
+
+    //checks diagonals
+    //left diagonal
+    
+    if(boardState[0][0] && boardState[0][0] == boardState[1][1] && boardState[0][0] == boardState[2][2]){
+        setWinner(boardState[0][0]);
+        return;
+    }
+    
+    //right diagonal
+    if(boardState[0][2] && boardState[0][2] == boardState[1][1] && boardState[0][2] == boardState[2][0]){
+        setWinner(boardState[0][2]);
+        return;
+    }
   }
 
 
   return <>
     <div className="h-screen flex items-center justify-center">
 
-      <div className="w-[300px] h-[300px] relative">
+      
+      <div className="w-[300px]">
+      <h1>
+        {winner ? `Winner: ${winner}` : gameSteps.length < 9 ? `Next player: ${currentTurn()}` : 'Game over'}
+      </h1>
         {boardState.map((row, rowIdx) => {
           return (
           <div className="board-row" key={rowIdx}>
             {row.map((cell, colIdx) => {
-              return <Square value={cell} key={colIdx} handleClick={() => handleClick(rowIdx, colIdx)}/>
+              return <Square value={cell} key={colIdx} handleClick={() => placeMarker(rowIdx, colIdx)}/>
             })}
           </div>
           );
