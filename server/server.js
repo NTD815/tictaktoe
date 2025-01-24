76b081502e7097b218e958362ef91b1894c357b0
manjeetnethues <manjeet.kumar@123789.org>
2025-01-24 19:34:30 +0530

    initial server setup

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, { cors: { origin: "*" } });
+
+io.on("connection", (socket) => {
+  console.log("User connected", socket.id);
+});
+
+httpServer.listen(4000);
